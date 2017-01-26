a1ee75f86f87ed79099ae0b8e5bebdc2ae1f5e0b
Pedro Lizarraga <plizarraga@fender.com>
2017-01-25 18:47:51 -0800

    Add Navigation

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -13,14 +13,14 @@ var Navigation = () => {
             <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
           </li>
           <li>
-            <Link to="/" activeClassName="active-link">Countdown</Link>
+            <Link to="/countdown" activeClassName="active-link">Countdown</Link>
           </li>
         </ul>
       </div>
       <div className="top-bar-right">
         <ul className="menu">
           <li className="menu-text">
-            Created by <a href="http://www.mead.io" target="_blank">Andrew Mead</a>
+            Created by <a href="https://github.com/plizarraga" target="_blank">Pedro Lizarraga</a>
           </li>
         </ul>
       </div>
--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,8 @@ module.exports = {
     root: __dirname,
     alias: {
       Main: 'app/components/Main.jsx',
+      Timer: 'app/components/Timer.jsx',
+      Countdown: 'app/components/Countdown.jsx',
       applicationStyles: 'app/styles/app.scss',
       Navigation: 'app/components/Navigation.jsx'
     },
